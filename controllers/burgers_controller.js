e500c68676b72b80b253e5d7ac862bb44cf3ec73
unknown <maryamshokoohhi@gmail.com>
2018-08-29 16:14:30 -0700

    initioal commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,45 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Get all burgers in burger database and render on page.
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+// Post new burger to database and refesh page to see it.
+router.post("/", function (req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function () {
+        res.redirect("/");
+    });
+});
+
+// Mark burger as devoured in database.
+// Refresh page to move it to devoured list.
+router.put("/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function () {
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,35 @@
+// Require these npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use("/public", express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Override with POST having ?_method=UPDATE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function () {
+    console.log("Listening on PORT " + port);
+});
